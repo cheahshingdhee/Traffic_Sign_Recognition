b443350662d1a8293dd165c02c8e1c8d3e3f143d
AhMing <keeshengming@outlook.com>
2021-09-10 16:59:43 +0800

    Add preprocess function

--- utility.py
+++ utility.py
@@ -5,3 +5,9 @@ def rescale_image(image, scale):
     width = int(image.shape[1] * scale)
     height = int(image.shape[0] * scale)
     return cv.resize(image, (width, height))
+
+
+def preprocess(img):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)).apply(img)
+    return img / 255
