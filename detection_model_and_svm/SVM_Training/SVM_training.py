4573c10a189aa66ef7b85a345e02b9e24334b89c
AhMing <keeshengming@outlook.com>
2021-09-10 22:35:08 +0800

    Move the package import to the top

--- detection_model_and_svm/SVM_Training/SVM_training.py
+++ detection_model_and_svm/SVM_Training/SVM_training.py
@@ -1,86 +1,83 @@
 import os
 import numpy as np
 import cv2
+import pickle
 from skimage.feature import hog
 from sklearn.metrics import roc_curve
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 
 print("done")
 
+
 def HOG(img):
-    fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8),cells_per_block=(2, 2), visualize=True, multichannel=True,)
-    return hog_image,fd
+    fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True,
+                        multichannel=True, )
+    return hog_image, fd
 
 
-path= "../../dataset"
+path = "../../dataset"
 
-count=0
-images=[]
-classNo=[]
+count = 0
+images = []
+classNo = []
 myList = os.listdir(path)
-print("Total class detected : ",len(myList))
+print("Total class detected : ", len(myList))
 
-noOfClasses=len(myList)
-for x in range(0,len(myList)):
-    myPicList=os.listdir(path+"/"+str(count))
+noOfClasses = len(myList)
+for x in range(0, len(myList)):
+    myPicList = os.listdir(path + "/" + str(count))
     for y in myPicList:
-        curImg=cv2.imread(path+"/"+str(count)+"/"+y)
-        curImg=cv2.resize(curImg,(32,32)) # resize the images
-        curImg,fd=HOG(curImg) # generate HOG feature
+        curImg = cv2.imread(path + "/" + str(count) + "/" + y)
+        curImg = cv2.resize(curImg, (32, 32))  # resize the images
+        curImg, fd = HOG(curImg)  # generate HOG feature
         images.append(fd)
         classNo.append(count)
     print(count)
-    count +=1
-
+    count += 1
 
 clf = svm.SVC(probability=True)
 
-hog_feature=np.array(images)
-hog_output=np.array(classNo).reshape(-1,1)
+hog_feature = np.array(images)
+hog_output = np.array(classNo).reshape(-1, 1)
 print(hog_feature.shape)
 print(hog_output.shape)
 
-
-data_frame = np.hstack((hog_feature,hog_output))
+data_frame = np.hstack((hog_feature, hog_output))
 print(data_frame.shape)
 np.random.shuffle(data_frame)
-#What percentage of data you want to keep for training
+# What percentage of data you want to keep for training
 percentage = 70
-partition = int(len(hog_feature)*percentage/100)
-x_train, x_test = data_frame[:partition,:-1],  data_frame[partition:,:-1]
-y_train, y_test = data_frame[:partition,-1:].ravel() , data_frame[partition:,-1:].ravel()
+partition = int(len(hog_feature) * percentage / 100)
+x_train, x_test = data_frame[:partition, :-1], data_frame[partition:, :-1]
+y_train, y_test = data_frame[:partition, -1:].ravel(), data_frame[partition:, -1:].ravel()
 
+clf.fit(x_train, y_train)
 
-
-
-clf.fit(x_train,y_train)
-from sklearn.model_selection import cross_val_score
 
 y_pred = clf.predict(x_test)
-#Testing accuracy
-print("Accuracy: "+str(accuracy_score(y_test, y_pred)))
+# Testing accuracy
+print("Accuracy: " + str(accuracy_score(y_test, y_pred)))
 print('\n')
-
 
 # 5-fold Cross Validation
 scores = cross_val_score(clf, x_train, y_train, cv=5)
 print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
 
-
 #########################################################################################
-y_pred=[]
+y_pred = []
 Pred_Label = clf.predict(x_test)
 y_prob_pred_cnb = clf.predict_proba(x_test)
 Pred_Label_p = clf.predict_proba(x_test)
-Pred_Label_p = np.max(Pred_Label_p,axis=1)
+Pred_Label_p = np.max(Pred_Label_p, axis=1)
 
 # Confusion Matrix
 ConfusionM = confusion_matrix(np.array(y_test), Pred_Label)
-#Precision and Recall for multiclass
+# Precision and Recall for multiclass
 class_report = classification_report(np.array(y_test), Pred_Label)
 
 print(ConfusionM)
@@ -93,7 +90,7 @@ thresh = {}
 n_class = 9
 
 for i in range(n_class):
-    fpr[i], tpr[i], thresh[i] = roc_curve(y_test, y_prob_pred_cnb[:, i],pos_label=i)
+    fpr[i], tpr[i], thresh[i] = roc_curve(y_test, y_prob_pred_cnb[:, i], pos_label=i)
 
 # plotting ROC
 plt.plot(fpr[0], tpr[0], linestyle='--', color='orange', label='Class 0 vs Rest')
@@ -113,10 +110,6 @@ plt.show()
 
 
 
-
-
-
-import pickle
 # save the model#
 with open('SVM_MODEL_FINAL', 'wb') as f:
-    pickle.dump(clf,f)
+    pickle.dump(clf, f)
